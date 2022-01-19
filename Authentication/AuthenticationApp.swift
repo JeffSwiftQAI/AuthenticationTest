9eac3b4474457ba8454f20cb822909cdc303e5a5
Jeff Swift <jeff@theswifts.org>
2022-01-19 12:00:18 -0700

    Initial Commit

---
+++ Authentication/AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationApp.swift
+//  Authentication
+//
+//  Created by Developer on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
