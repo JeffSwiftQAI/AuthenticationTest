551ecbc996ca449b0c88d2e73a8fa98e6c220790
Jeff Swift <jeff@theswifts.org>
2022-02-03 13:24:51 -0600

    Added README file

---
+++ Authentication/AuthenticationTestApp.swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticationApp.swift
+//  Authentication test for the OAuth2PKCEAuthentication package
+//
+//  Correctly displays the log in screen for iPhone
+//  Does NOT correctly display login view for iPad
+//
+//  Created by Developer on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Authentication/README.md
@@ -0,0 +1,6 @@
+#  Authentication
+
+This "Test Jig" allows for verification of, and a pattern to configure and use, the OAuth2Authentication package
+
+This testbed is limited to testing authentication on an iPhone simulator and does NOT currently support an iPad simulator 
+
